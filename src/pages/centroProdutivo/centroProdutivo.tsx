7005ec8fe58620f0a6f9e60672464f78ef9b1bbf
iagorrr04 <iagorrr04@gmail.com>
2023-07-07 20:42:43 -0300

    fix: remove código comentado

--- src/pages/centroProdutivo/centroProdutivo.tsx
+++ src/pages/centroProdutivo/centroProdutivo.tsx
@@ -49,7 +49,6 @@ import { CentrosCadastrarDTO } from "./dtos/CentrosCadastrar.dto";
 import { CentrosListarDTO } from "./dtos/CentrosListar.dto";
 import { AuthContext } from "../../context/AuthProvider";
 import ValueMask from "../../shared/components/Masks/ValueMask";
-// import { VagasCentroProdutivoDTO } from "./dtos/VagasCentroProdutivo";
 import { VagasListarCentroDTO } from "./dtos/VagasListarCentro.dto";
 import { CentroInscritosDTO } from "./dtos/CentroInscritosDTO";
 import { FaList } from "react-icons/fa";
@@ -127,15 +126,11 @@ export function CentroProdutivo() {
   const handleCloseConfirmation = () => setOpenConfirmation(false);
   const methods = useForm();
   const [openConfirmation, setOpenConfirmation] = useState(false);
-  // const [selectedCentro, setSelectedCentro] = useState(null);
   const [Centro, setCentro] = useState(Object);
   const [id, setId] = useState<GridRowId>(0);
   const [openEdit, setOpenEdit] = useState(false);
   const [dataTable, setDataTable] = useState(Array<Object>);
   const [vaga, setVagas] = useState<VagasListarCentroDTO[]>([]);
-  // const para alterar vagas disponiveis
-  // const [inscrito, setInscrito] = useState(false);
-  // const [codigoCentro, setcodigoCentro] = useState<GridRowId>(0);
   const auth = useContext(AuthContext);
 
   const [openList, setOpenList] = useState(false);
@@ -186,8 +181,6 @@ export function CentroProdutivo() {
     const response = await gerarRelatorio(dadosAlunas);
 
     if (response.status === 201) {
-      // setOpen(false)
-      // queryClient.invalidateQueries('listar_centro')
       toast.success("Dados do relatorio salvos com sucesso!");
     } else {
       toast.error("Campos inválidos");
@@ -531,11 +524,9 @@ export function CentroProdutivo() {
               (item) => (item as any).id === params.id
             );
             if (selectedRow) {
-              // carregarCentro((selectedRow as any).idCentro);
               carregarAlunasDoCentro((selectedRow as any).idCentro);
               handleOpenExportar();
             }
-            // queryClient.invalidateQueries('listar_alunas_cadastradas');
           }}
         ></ActionButton>,
         <div>
